bdd4c9dc0e6449d84162689a7c37752010049bcd
wswdx <2819434743@qq.com>
2025-03-18 10:16:13 +0800

    enable auto downloading of vgg

--- model/losses.py
+++ model/losses.py
@@ -52,8 +52,8 @@ class LPIPS(nn.Module):
             param.requires_grad = False
 
     def load_from_pretrained(self, name="vgg_lpips"):
-        # ckpt = get_ckpt_path(name, "taming/modules/autoencoder/lpips")
-        ckpt = ".cache/vgg.pth"
+        ckpt = get_ckpt_path(name, "taming/modules/autoencoder/lpips")
+        #ckpt = ".cache/vgg.pth"
         self.load_state_dict(torch.load(ckpt, map_location=torch.device("cpu")), strict=False)
         print("loaded pretrained LPIPS loss from {}".format(ckpt))
 
